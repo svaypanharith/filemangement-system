30d26ae60959085b23d0fe9f15472d614a5b0017
svaypanharith <svaypanharith20@gmail.com>
2025-06-08 22:09:20 +0700

    refector code

--- src/app/dashboard/chatbot/page.tsx
+++ src/app/dashboard/chatbot/page.tsx
@@ -1,15 +1,21 @@
 "use client";
-import Chatbot from "@/components/chatbot/ChatBot";
+import ChatContent from "@/components/chatbot/ChatContent";
+import ChatBotForm from "@/components/chatbot/ChatBotForm";
 import { Sparkles } from "lucide-react";
 
 export default function ChatbotPage() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-2 w-full">
       <div className="flex items-center gap-2">
         <Sparkles className="w-5 h-5 text-purple-600" />
         <span className="text-2xl font-bold"> ASK TO AI</span>
       </div>
-      <Chatbot />
+      <div className="flex flex-col w-full justify-center items-center">
+        <div className="flex  overflow-y-auto h-[calc(90vh-100px)] ">
+          <ChatContent />
+        </div>
+      </div>
+      <ChatBotForm />
     </div>
   );
 }
