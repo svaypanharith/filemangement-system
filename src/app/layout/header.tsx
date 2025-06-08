ff024f09c0230b193e876dfbfac38b23480e42bc
svaypanharith <svaypanharith20@gmail.com>
2025-06-08 22:10:19 +0700

    refector code

--- src/app/layout/header.tsx
+++ src/app/layout/header.tsx
@@ -6,8 +6,8 @@ import SwitchLanguage from "@/components/share/SwitchLanguage";
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between">
-      <SidebarTrigger />
+    <div className="flex items-center justify-between sticky top-0  z-10 dark:bg-slate-950 ">
+      <SidebarTrigger className="cursor-pointer" />
       <div className="flex items-center gap-4">
         <SwitchLanguage />
         <span>Svay Phanharith</span>
