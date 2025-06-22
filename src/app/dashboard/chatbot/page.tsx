f6e2415b6646028b82267c72fabea7e6a190cd0b
Svay Panharith <svaypanharith@Svays-MacBook-Air.local>
2025-06-22 16:26:16 +0700

    update translation

--- src/app/dashboard/chatbot/page.tsx
+++ src/app/dashboard/chatbot/page.tsx
@@ -2,13 +2,15 @@
 import ChatContent from "@/components/chatbot/ChatContent";
 import ChatBotForm from "@/components/chatbot/ChatBotForm";
 import { Sparkles } from "lucide-react";
+import { useTranslation } from "react-i18next";
 
 export default function ChatbotPage() {
+  const { t } = useTranslation();
   return (
     <div className="flex flex-col gap-2 w-full">
       <div className="flex items-center gap-2">
         <Sparkles className="w-5 h-5 text-purple-600" />
-        <span className="text-2xl font-bold"> ASK TO AI</span>
+        <span className="text-2xl font-bold">{t("chatbot.ask_to_ai")}</span>
       </div>
       <div className="flex flex-col w-full justify-center items-center">
         <div className="flex  overflow-y-auto h-[calc(90vh-100px)] ">
--- src/app/dashboard/document/page.tsx
+++ src/app/dashboard/document/page.tsx
@@ -2,13 +2,15 @@
 
 import { Folder } from "lucide-react";
 import Document from "@/components/document/document";
+import { useTranslation } from "react-i18next";
 export default function DocumentPage() {
+  const { t } = useTranslation();
   return (
     <div className="flex flex-col gap-10  ">
       <div className="flex flex-col gap-4">
         <div className="flex items-center gap-2">
           <Folder className="w-5 h-5" />
-          <h1 className="text-2xl font-bold">Document</h1>
+          <h1 className="text-2xl font-bold">{t("document.document")}</h1>
         </div>
       </div>
       <Document />
